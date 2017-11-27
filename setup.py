f9078526eaecde643e5a16c4698885af6603cc39
Wieland Hoffmann <WHoffman@de.ibm.com>
2017-11-27 10:48:11 +0100

    whitespace-cleanup

--- setup.py
+++ setup.py
@@ -22,44 +22,44 @@ with open('README.rst', 'r', encoding='utf-8') as f:
     longdesc = f.read()
 
 classifiers = [
-	    # How mature is this project? Common values are
-	    #   3 - Alpha
-	    #   4 - Beta
-	    #   5 - Production/Stable
-	    'Development Status :: 4 - Beta',
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
 
-	    # Indicate who your project is intended for
-	    'Intended Audience :: Developers',
-	    'Intended Audience :: Education',
-	    'Intended Audience :: End Users/Desktop',
-	    'Intended Audience :: Information Technology',
-	    'Intended Audience :: Science/Research',
-	    'Topic :: Software Development :: Build Tools',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Build Tools',
 
-	    # Pick your license as you wish (should match "license" above)
-	    'License :: OSI Approved :: BSD License',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
 
-	    'Operating System :: MacOS :: MacOS X',
+        'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
 
         'Natural Language :: English',
 
-	    # Specify the Python versions you support here. In particular, ensure
-	    # that you indicate whether you support Python 2, Python 3 or both.
-	    'Programming Language :: Python :: 2.7',
-	    'Programming Language :: Python :: 3',
-	    'Programming Language :: Python :: 3.2',
-	    'Programming Language :: Python :: 3.3',
-	    'Programming Language :: Python :: 3.4',
-	    'Programming Language :: Python :: 3.5',
-	    #'Programming Language :: Python :: 3.6',
-	    'Programming Language :: Python :: Implementation :: CPython',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        #'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
 
-	    'Topic :: Database',
-	    'Topic :: Scientific/Engineering',
-	    'Topic :: Software Development'
-	  ]
+        'Topic :: Database',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development'
+      ]
 
 setup(name='ibmdbpy',
       version='0.1.4',
@@ -67,8 +67,8 @@ setup(name='ibmdbpy',
       # optional are jaydebeapi, pytest, sphinx, bokeh
       extras_require={
         'jdbc':['jaydebeapi>=0.2.0,<1.0.0'],
-      	'test':['pytest'],
-      	'doc':['sphinx']
+        'test':['pytest'],
+        'doc':['sphinx']
       },
       description='A Pandas-like SQL-wrapper for in-database analytics with IBM dashDB/DB2.',
       long_description=longdesc,
@@ -80,6 +80,5 @@ setup(name='ibmdbpy',
       keywords='data analytics database development IBM dashDB DB2 pandas scikitlearn scalability machine-learning knowledge discovery',
       packages=find_packages(exclude=['docs', 'tests*']),
       package_data={
-      	'ibmdbpy.sampledata': ['*.txt']}
+        'ibmdbpy.sampledata': ['*.txt']}
      )
-
