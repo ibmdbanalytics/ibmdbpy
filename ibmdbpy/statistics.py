9b60f3f9ed0d3e08ef554e9d6c29f6a60e3e65e9
ToniBollinger <Toni.Bollinger@de.ibm.com>
2020-03-13 10:42:51 +0100

    Update ibmdbpy/statistics.py
    Co-Authored-By: Wieland Hoffmann <mineo@users.noreply.github.com>

--- ibmdbpy/statistics.py
+++ ibmdbpy/statistics.py
@@ -637,7 +637,7 @@ def cov(idadf, other = None):
         raise TypeError("cov() missing 1 required positional argument: 'other'")
 
     columns = idadf._get_numerical_columns()
-    if not columns or len(columns) < 2:
+    if len(numerical_columns) < 2 :
         print(idadf.name + " has less than two numeric columns")
         return
 
