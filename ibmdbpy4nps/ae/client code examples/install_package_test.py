b21a3d9d946e6c209a3327aeaf45fe606797ca3d
Vinay kumar Reddy <Vinay.Kasireddy@ibm.com>
2021-05-20 15:53:46 +0530

    stock prediction example

--- ibmdbpy4nps/ae/client code examples/install_package_test.py
+++ ibmdbpy4nps/ae/client code examples/install_package_test.py
@@ -4,6 +4,6 @@ from ibmdbpy4nps.ae.install import NZInstall
 
 idadb = IdaDataBase('weather', 'admin', 'password')
 
-nzinstall = NZInstall(idadb, package_name='scikit-learn')
+nzinstall = NZInstall(idadb, package_name='pandas')
 print(nzinstall.getResultCode())
 
